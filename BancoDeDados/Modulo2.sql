34bb998b8b882bb3443a57653eb1c20359cdfc1e
henriquesbral <henriquesbral@gmail.com>
2025-01-28 08:23:10 -0300

    Subindo o modulo2 de banco de dados

---
+++ BancoDeDados/Modulo2.sql
@@ -0,0 +1,36 @@
+USE DesenvolvedorIO
+GO
+
+/*CREATE TABLE Alunos 
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Nome VARCHAR(80) NOT NULL,
+	CPF CHAR(11) NOT NULL,
+	Data_Nascimento DATE,
+	Ativo BIT DEFAULT 1
+)
+
+INSERT INTO Alunos 
+(
+Nome
+,CPF
+,Data_Nascimento
+)
+VALUES 
+(
+'Mariano Antonio Silveira', '95841232615', '1986-07-06'
+),
+(
+'Antonio Sobral', '12345678901', '2001-12-06'
+),
+(
+'Lucas silva oliveira Sobral', '00000000014', '1954-07-06'
+)
+
+SELECT * FROM Alunos 
+
+UPDATE Alunos SET Nome = 'Lucas Antonio Oliveira Sobral' WHERE Id = 3
+
+DELETE Alunos WHERE Id = 4
+*/
+
